259eb3a1dd25725daa1fdc04a6f94a68a78914ab
JasonXChen <jasonxchen888@gmail.com>
2022-11-14 23:18:06 -0500

    Set up FastAPI with HTTPX

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from httpx import AsyncClient
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the home route"}
+
+@app.get("/httpx")
+async def httpx_test():
+    async with AsyncClient(base_url="https://httpbin.org") as ac:
+        response = await ac.get("/get")
+    # alt: response = await AsyncClient().get("https://httpbin.org/get")
+    return response.json()
