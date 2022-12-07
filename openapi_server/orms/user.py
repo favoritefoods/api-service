015b599fcffbab4005cd822c45681fa80677aac9
JasonXChen <jasonxchen888@gmail.com>
2022-12-07 16:31:48 -0500

    Include env file in repo and remove extra env variable getters

--- openapi_server/orms/user.py
+++ openapi_server/orms/user.py
@@ -9,8 +9,6 @@ class DbUser(Model):
     class Meta:
         table_name: str = "User"
         host: str = os.environ.get("AWS_DYNAMODB_HOST")
-        aws_access_key_id: str = os.environ.get("AWS_ACCESS_KEY_ID")
-        aws_secret_access_key: str = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
     username: UnicodeAttribute = UnicodeAttribute(hash_key=True, default="")
     first_name: UnicodeAttribute = UnicodeAttribute(default="")
