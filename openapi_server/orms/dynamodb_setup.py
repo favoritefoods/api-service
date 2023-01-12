a212437f8e306b9488e96f37d17f8ef2f29c098e
JasonXChen <jasonxchen888@gmail.com>
2023-01-12 17:23:37 -0500

    Add Review and Restaurant tables to DB setup

--- openapi_server/orms/dynamodb_setup.py
+++ openapi_server/orms/dynamodb_setup.py
@@ -1,6 +1,14 @@
 from openapi_server.orms.user import DbUser
+from openapi_server.orms.review import DbReview
+from openapi_server.orms.restaurant import DbRestaurant
 
 
 def dynamodb_setup():
     if not DbUser.exists():
         DbUser.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+    if not DbReview.exists():
+        DbReview.create_table(read_capacity_units=1, write_capacity_units=1, wait=True)
+    if not DbRestaurant.exists():
+        DbRestaurant.create_table(
+            read_capacity_units=1, write_capacity_units=1, wait=True
+        )
